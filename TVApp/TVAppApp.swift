94d36fd64fc5d725c7c763546c7a4b0873574af4
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2022-04-12 13:53:15 -0300

    Initial Commit

---
+++ TVApp/TVAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVAppApp.swift
+//  TVApp
+//
+//  Created by Raphael Alkamim on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TVAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
